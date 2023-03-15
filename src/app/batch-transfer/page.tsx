0f788200f5a8da0a0098b7879e165aceedb5ef35
benson <naivebenson@gmail.com>
2023-03-15 19:13:18 +0800

    feat: router

---
+++ src/app/batch-transfer/page.tsx
@@ -0,0 +1,3 @@
+export default function BatchTransfer () {
+  return <div>BatchTransfer</div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 // import { Proto } from 'next/font/google'
 
+import Link from "next/link";
+
 // const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  return <div>home page</div>
+  return <div>
+    <Link href='/batch-transfer'>批量转账</Link>
+  </div>
 }
