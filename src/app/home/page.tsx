9b6e31eed12c9d9311afd39763c836f1243eac83
lybenson <naivebenson@gmail.com>
2023-03-19 16:49:33 +0800

    feat: disable cache

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,19 +1,21 @@
+// server component
 export default async function Home () {
   const data = await getData()
   return <div className="font-bold font-pixel p-2 text-success text-xl8">{data.count}</div>
 }
 
 const getData = async () => {
-  console.log('getData')
-  
-  await stop()
-  return { count: 200 } 
+  await pause()
+  return { count: Math.random() } 
 }
 
-function stop () {
+function pause () {
   return new Promise<void>((resolve) => {
     setTimeout(() => {
       resolve()
     }, 2000)
   })
 }
+
+// disable static cache
+export const revalidate = 0
